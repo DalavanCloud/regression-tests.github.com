98964a649445c8b5c4877f59a27a4314418ed573
Paul Hammant <paul@hammant.org>
2011-12-22 08:35:38 -0600

    fix up illustration of issue

--- issues/2915.groovy
+++ issues/2915.groovy
@@ -1,5 +1,5 @@
 @Grapes([
- @Grab("org.seleniumhq.selenium:selenium-java:2.13.0"),
+ @Grab("org.seleniumhq.selenium:selenium-java:2.14.0"),
  @GrabExclude('xml-apis:xml-apis')
 ])
 
@@ -18,16 +18,18 @@ if (args.length == 0) {
 
 driver.get domain + "/demo/2915/index.html"
 
-def pwField = driver.findElement(By.id("cheddarCheeseLoginPassword"))
-pwField.sendKeys("Selenium")
-
-
 try {
-  def pwField2 = driver.findElement(By.id("cheddarCheeseLoginPassword"))
-  pwField.getText() // barfs here if bug is present
-  println "Issue 2915 not present"
+  def pwField = driver.findElement(By.id("cheddarCheeseLoginPassword"))
+  pwField.sendKeys("Selenium") // this does not barf.
+  String pw = pwField.getText() // barfs here if bug is present
+  if (pw == "Selenium") {
+      println "Issue 2915 not present, field value retrieved" 
+  } else {
+      println "field value retrieved but it is incorrect: '$pw'" // it SOMETIMES does this path when the FF screen is hidden. 
+  } 
 } catch(StaleElementReferenceException e) {
-  println "Issue 2915 present"
+  println "Issue 2915 present, get via workaround? " 
+  println "locate it again, and get the field value: '" + driver.findElement(By.id("cheddarCheeseLoginPassword")).getText() + "'"
 } finally {
   driver.quit()
 }
